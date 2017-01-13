6d4dc8d8fd7c483327a0a8d1186d4b27cd534c12
mkobar <mike@exlorev.com>
2017-01-13 18:32:05 -0500

    track status of authentication

--- app/app.js
+++ app/app.js
@@ -4,8 +4,37 @@
 angular.module('myPeeps', [
   'ngRoute',
   'firebase',
+
+  'myApp.config',
+  'myApp.security',
+
+  'firebase.appauth',
+
+  'myApp.home',
+  'myApp.account',
+  'myApp.login',
+
   'myPeeps.peeps'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/peeps'});
+])
+
+.config(['$routeProvider', function ($routeProvider) {
+    // Initialize Firebase
+    var config = {
+       apiKey: PRIVATE.firebase_api,
+       authDomain: PRIVATE.firebase_authDomain,
+       databaseURL: PRIVATE.firebase_databaseURL
+    };
+    firebase.initializeApp(config);
+
+    $routeProvider.otherwise({
+      redirectTo: '/peeps'
+      //redirectTo: '/home'
+    });
+}])
+  
+.run(['$rootScope', 'Auth', function($rootScope, Auth) {
+    // track status of authentication
+    Auth.$onAuthStateChanged(function(user) {
+      $rootScope.loggedIn = !!user;
+    });
 }]);
